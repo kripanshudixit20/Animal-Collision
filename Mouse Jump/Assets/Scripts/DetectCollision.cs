e48e003f993e637b7e2083996e66ff65bfeef559
Kripanshu Dixit <66563984+kripanshudixit20@users.noreply.github.com>
2020-10-09 10:45:32 +0530

    Initial commit
    This is my first 2D Unity project. Here, six animal prefabs spawn randomly on left mouse click, when two same prefabs collide, they get destroyed.

---
+++ Mouse Jump/Assets/Scripts/DetectCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour
+{
+    Vector3 newScale;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (gameObject.CompareTag(collision.gameObject.tag))
+        {
+            Debug.Log($"{gameObject.tag} collided with {collision.gameObject.tag}");
+            newScale = gameObject.transform.localScale;
+            newScale.x *= 2;
+            newScale.y *= 2;
+            transform.localScale = newScale;
+            Destroy(collision.gameObject);
+        }
+       
+    }
+}
---
+++ Mouse Jump/Assets/Scripts/DetectMouseClick.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectMouseClick : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Animal0;
+    [SerializeField]
+    GameObject Animal1;
+    [SerializeField]
+    GameObject Animal2;
+    [SerializeField]
+    GameObject Animal3;
+    [SerializeField]
+    GameObject Animal4;
+    [SerializeField]
+    GameObject Animal5;
+
+    GameObject currentCharacter;
+
+    int randomX, randomY;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Spawn a character at the middle of the screen
+        currentCharacter = Instantiate<GameObject>(Animal1, Vector3.zero, Quaternion.identity);
+
+        randomX = Random.Range(0, 10);
+        randomY = Random.Range(0, 10);
+
+        currentCharacter.GetComponent<Rigidbody2D>().AddForce(new Vector2(randomX, randomY), ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the left mouse button is clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Get the position of the currently instantiated character
+            Vector3 currentPosition = currentCharacter != null ? currentCharacter.transform.position : Vector3.zero;
+
+            // Instantiating a random character
+            int randomNumber = Random.Range(0, 6);
+            if (randomNumber == 0)
+            {
+                currentCharacter = Instantiate(Animal0, currentPosition, Quaternion.identity);
+
+                randomX = Random.Range(0, 10);
+                randomY = Random.Range(0, 10);
+
+                currentCharacter.GetComponent<Rigidbody2D>().AddForce(new Vector2(randomX, randomY), ForceMode2D.Impulse);
+            }
+            else if (randomNumber == 0)
+            {
+                currentCharacter = Instantiate(Animal1, currentPosition, Quaternion.identity);
+
+                randomX = Random.Range(0, 10);
+                randomY = Random.Range(0, 10);
+
+                currentCharacter.GetComponent<Rigidbody2D>().AddForce(new Vector2(randomX, randomY), ForceMode2D.Impulse);
+            }
+            else if (randomNumber == 1)
+            {
+                currentCharacter = Instantiate(Animal2, currentPosition, Quaternion.identity);
+
+                randomX = Random.Range(0, 10);
+                randomY = Random.Range(0, 10);
+
+                currentCharacter.GetComponent<Rigidbody2D>().AddForce(new Vector2(randomX, randomY), ForceMode2D.Impulse);
+            }
+            else if (randomNumber == 2)
+            {
+                currentCharacter = Instantiate(Animal3, currentPosition, Quaternion.identity);
+
+                randomX = Random.Range(0, 10);
+                randomY = Random.Range(0, 10);
+
+                currentCharacter.GetComponent<Rigidbody2D>().AddForce(new Vector2(randomX, randomY), ForceMode2D.Impulse);
+            }
+            else if (randomNumber == 3)
+            {
+                currentCharacter = Instantiate(Animal5, currentPosition, Quaternion.identity);
+
+                randomX = Random.Range(0, 10);
+                randomY = Random.Range(0, 10);
+
+                currentCharacter.GetComponent<Rigidbody2D>().AddForce(new Vector2(randomX, randomY), ForceMode2D.Impulse);
+            }
+            else if (randomNumber == 4)
+            {
+                currentCharacter = Instantiate(Animal4, currentPosition, Quaternion.identity);
+
+                randomX = Random.Range(0, 10);
+                randomY = Random.Range(0, 10);
+
+                currentCharacter.GetComponent<Rigidbody2D>().AddForce(new Vector2(randomX, randomY), ForceMode2D.Impulse);
+            }
+            else if (randomNumber == 5)
+            {
+                currentCharacter = Instantiate(Animal5, currentPosition, Quaternion.identity);
+
+                randomX = Random.Range(0, 10);
+                randomY = Random.Range(0, 10);
+
+                currentCharacter.GetComponent<Rigidbody2D>().AddForce(new Vector2(randomX, randomY), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
